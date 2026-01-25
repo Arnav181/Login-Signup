d12caa12f783a648562f6edd1e2c2cec8155d360
Arnav <arnavwadhwa016@gmail.com>
2026-01-25 16:56:02 +0700

    final

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import Task from "../models/Task.js";
+import Task from "../models/task.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
